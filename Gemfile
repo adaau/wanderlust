c98e4fd05a5dfde805dd5ca48b9c976c8224315b
adaau <ada.au.cheng@gmail.com>
2015-12-08 21:10:46 +0800

    index and show Places api; Gemfile; gitignore

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+gem 'rails', '4.2.4'
+gem 'pg'
+gem 'bcrypt', '~> 3.1.2'
+gem 'jbuilder', '~> 2.0'
+gem 'aws-sdk', '< 2.0'
+
+
+# Assets
+gem 'jquery-rails'
+gem 'coffee-rails', '~> 4.1.0'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+
+gem 'bootstrap-sass'
+gem 'bootstrap_form'
+gem 'wannabe_bool'
+gem 'paperclip', "~> 4.3"
+gem 'turbolinks'
+
+group :development, :test do
+  gem 'better_errors'
+  gem 'pry-rails'
+  gem 'binding_of_caller'
+  gem 'dotenv-rails'
+end
+
+group :development do
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
+group :production do
+  gem 'rails_12factor'
+end
---
+++ db/seeds.rb
@@ -0,0 +1,42 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Place.destroy_all
+
+  places = Place.create([
+    {name: "Tokyo",
+    country: "Japan",
+    },
+    {name: "Sydney",
+    country: "Australia",
+    },
+    {name: "Seoul",
+    country: "Korea",
+    },
+    {name: "London",
+    country: "United Kingdom",
+    },
+    {name: "Venice",
+    country: "Italy",
+    },
+    {name: "New York",
+    country: "USA",
+    },
+    {name: "San Francisco",
+    country: "USA",
+    },
+    {name: "Vancouver",
+    country: "Canada",
+    },
+    {name: "Singapore",
+    country: "Singapore",
+    },
+    {name: "Shanghai",
+    country: "China",
+    }
+  ])
