9cdf19dd008f7d56fef817d45125a66d4a1c5043
adaau <ada.au.cheng@gmail.com>
2015-12-10 17:39:52 +0800

    small changes before deploying to Heroku

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module App
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end
