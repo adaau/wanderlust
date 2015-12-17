f70e5aaed3acf2cb31306251ebc2e51a76586db0
adaau <ada.au.cheng@gmail.com>
2015-12-17 14:12:58 +0800

    fixed bookmark index and delete

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,9 +3,10 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   validates :list, presence: true
 
-  before_save :update_place_name
+  before_save :update_columns
 
-  def update_place_name
+  def update_columns
     self.place_name = self.place.name
+    self.list_number = Date::MONTHNAMES.index(self.list)
   end
 end
